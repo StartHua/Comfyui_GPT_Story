8766b6f53e8b5005c3236047479c793600d0a1f5
StartHua <StartHua@users.noreply.github.com>
2023-12-16 00:42:55 +0800

    Add files via upload

---
+++ Comfyui-Mine/__init__.py
@@ -0,0 +1,13 @@
+# from .mine_nodes import *
+from .CXH_GPT import *
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    "CXH_GPT":CXH_GPT,
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "CXH_GPT": "CXH_GPT",
+}
